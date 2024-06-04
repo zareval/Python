198ab6224e3fb7aa41880ef2173a8d8d7809649a
kevinchi <kevinchi78@gmail.com>
2024-06-04 09:39:40 -0500

    azure

--- inges_project/settings.py
+++ inges_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^%_&d7d+6d&!e4%j($czvc7*ir*lg3vage*w1n6fk^6xuwz9y+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','ingesweb.azurewebsites.net']
 
 
 # Application definition
